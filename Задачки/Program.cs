4b9dae76562c40d6e907cea44879424ada0819c1
Ya-Va-A <yavalery@gmail.com>
2021-11-07 10:46:03 +0300

    tasks

---
+++ Задачки/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: по двум заданным числам проверить является ли первое квадратом второго
+int a = 16;
+int b = 4;
+
+bool SQR (int a, int b)
+{
+if(a==b*b)
+{
+    return true;
+}
+return false;
+}
+
+Console.WriteLine($"{SQR (a,b)} a: {a} , b: {b} ");
