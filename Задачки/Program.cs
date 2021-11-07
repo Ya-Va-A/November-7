c169f57579d9d3ece31c13daa461d2501ae205a9
Ya-Va-A <yavalery@gmail.com>
2021-11-07 11:54:10 +0300

    task 3

--- Задачки/Program.cs
+++ Задачки/Program.cs
@@ -1,14 +1,32 @@
 ﻿// Задача 1: по двум заданным числам проверить является ли первое квадратом второго
-int a = 16;
-int b = 4;
+// int a = 16;
+//int b = 4;
 
-bool SQR (int a, int b)
-{
-if(a==b*b)
-{
-    return true;
-}
-return false;
-}
+//bool SQR (int a, int b)
+//{
+//if(a==b*b)
+//{
+//    return true;
+//}
+//return false;
+//}
 
-Console.WriteLine($"{SQR (a,b)} a: {a} , b: {b} ");
+//Console.WriteLine($"{SQR (a,b)} a: {a} , b: {b} ");
+
+// Задача 3 - по заданомму номеру лня недели вывести его название
+//Зададим как метод
+
+Console.WriteLine($"Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void DayWeek(int number)
+{
+     if (number == 1) Console.WriteLine ("понедельник");
+     if (number == 2) Console.WriteLine ("вторник");
+     if (number == 3) Console.WriteLine ("среда");
+     if (number == 4) Console.WriteLine ("четверг");
+     if (number == 5) Console.WriteLine ("пятница");
+     if (number == 6) Console.WriteLine ("суббота");
+     if (number == 7) Console.WriteLine ("воскресенье");
+}
+  DayWeek(number);
